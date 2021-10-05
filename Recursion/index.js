98aaa56358fa04b92fcb97083cca84ae2706d6d9
kadum komut <kadumeliza@gmail.com>
2021-10-05 15:55:20 +0530

    commit

--- Recursion/index.js
+++ Recursion/index.js
@@ -1,9 +1,3 @@
-const RecursiveSum = (n) =>{
-    if(n===1){
-        return 1;
-    }else {
-        return RecursiveSum(n-1)+n;
-    }
-}
-
-console.log(RecursiveSum(5))
+const gridPath = [
+    []
+]
--- example.js
+++ example.js
@@ -1,12 +1,25 @@
-class Node {
-    constructor(data, next=null){
-        this.data = data;
-        this.next = next;
-    }
-}
+var uniqueOccurrences = function(arr) {
+  let map1 = new Map();
+  let map2 = new Map();
+  let flag = false;
+  for(let x of arr){
+      if(map1.has(x)){
+          map1.set(x, parseInt(map1.get(x))+1)
+          flag = true;
+          continue;
+      }
+      map1.set(x,1);
+  }
+  if(!flag) return false;
+  map1.forEach((value)=>{
+      if(map2.has(parseInt(value))) {
+        flag = false
+      };
+      map2.set(parseInt(value),value)
+  })
+  if(!flag) return false;
+  return true;
+};
 
-class LinkedList {
-    constructor(data){
-        this.head = new Node(data);
-    }
-}
+let arr = [3,5,-2,-3,-6,-6];
+console.log(uniqueOccurrences(arr))
